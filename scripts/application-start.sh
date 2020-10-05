67d749485d929afe45fae3cbead72b8d1ab522c5
raiders032 <nameks@naver.com>
2020-10-05 16:35:42 +0900

    script 수정

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-REPOSITORY = /home/ubuntu/build
-PROJECT_NAME = sprint1
+REPOSITORY=/home/ubuntu/build
+PROJECT_NAME=sprint1
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 cd $REPOSITORY
 # 실행 중이면 종료하기 위해서 현재 수행 중인 프로세스id를 찾습니다.
 # springboot2-webservice으로 된 다른 프로그램들이 있을 수 있어 springboot2-webservice된 jar 프로세스를 찾은 뒤 id를 찾습니다(awk '{print $1}').
-CURRENT_PID = $(pgrep -fl springboot2-webservice | grep java | awk '{print $1}')
+# shellcheck disable=SC1068
+CURRENT_PID=$(pgrep -fl springboot2-webservice | grep java | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
@@ -21,7 +22,7 @@ fi
 
 echo "> 새 애플리케이션 배포"
 
-JAR_NAME = $(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR name: JAR_NAME"
 echo "> $JAR_NAME에 실행 권한 추가"
@@ -31,6 +32,3 @@ echo "> $JAR_NAME 실행"
 
 nohup java -jar \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
-~
-~
-~
