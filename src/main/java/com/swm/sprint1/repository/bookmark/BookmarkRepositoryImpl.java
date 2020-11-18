1261f91591497b761462d8c08a20611bc4127fb8
raiders032 <nameks@naver.com>
2020-11-18 16:24:20 +0900

    북마크 정렬 기능 추가

--- src/main/java/com/swm/sprint1/repository/bookmark/BookmarkRepositoryImpl.java
+++ src/main/java/com/swm/sprint1/repository/bookmark/BookmarkRepositoryImpl.java
@@ -1,6 +1,8 @@
 package com.swm.sprint1.repository.bookmark;
 
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.NumberPath;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.swm.sprint1.domain.Bookmark;
@@ -35,6 +37,61 @@ public class BookmarkRepositoryImpl implements BookmarkRepositoryCustom{
                 .leftJoin(restaurant.userLikings, userLiking)
                 .where(bookmark.user.id.eq(userId))
                 .groupBy(restaurant.id)
+                .fetch();
+
+        JPAQuery<Bookmark> countQuery = queryFactory
+                .select(bookmark)
+                .from(bookmark)
+                .join(bookmark.restaurant, restaurant)
+                .leftJoin(restaurant.userLikings, userLiking)
+                .where(bookmark.user.id.eq(userId))
+                .groupBy(restaurant.id);
+
+        return PageableExecutionUtils.getPage(contents, pageable, countQuery::fetchCount);
+    }
+
+    @Override
+    public Page<BookmarkResponseDto> findDtosByUserIdOrderByLike(Long userId, Pageable pageable) {
+        NumberPath<Long> aliasLike = Expressions.numberPath(Long.class, "likecnt");
+        List<BookmarkResponseDto> contents = queryFactory
+                .select(Projections.fields(BookmarkResponseDto.class,
+                        bookmark.id, restaurant.id.as("restaurantId"), restaurant.name,
+                        restaurant.thumUrl, userLiking.id.count().as("likecnt"),
+                        restaurant.latitude, restaurant.longitude
+
+                ))
+                .from(bookmark)
+                .join(bookmark.restaurant, restaurant)
+                .leftJoin(restaurant.userLikings, userLiking)
+                .where(bookmark.user.id.eq(userId))
+                .groupBy(restaurant.id)
+                .orderBy(aliasLike.desc())
+                .fetch();
+
+        JPAQuery<Bookmark> countQuery = queryFactory
+                .select(bookmark)
+                .from(bookmark)
+                .join(bookmark.restaurant, restaurant)
+                .leftJoin(restaurant.userLikings, userLiking)
+                .where(bookmark.user.id.eq(userId))
+                .groupBy(restaurant.id);
+
+        return PageableExecutionUtils.getPage(contents, pageable, countQuery::fetchCount);
+    }
+
+    @Override
+    public Page<BookmarkResponseDto> findDtosByUserIdOrderById(Long userId, Pageable pageable) {
+        List<BookmarkResponseDto> contents = queryFactory
+                .select(Projections.fields(BookmarkResponseDto.class,
+                        bookmark.id, restaurant.id.as("restaurantId"), restaurant.name,
+                        restaurant.thumUrl, userLiking.id.count().as("likecnt"),
+                        restaurant.latitude, restaurant.longitude))
+                .from(bookmark)
+                .join(bookmark.restaurant, restaurant)
+                .leftJoin(restaurant.userLikings, userLiking)
+                .where(bookmark.user.id.eq(userId))
+                .groupBy(restaurant.id)
+                .orderBy(bookmark.id.desc())
                 .fetch();
 
         JPAQuery<Bookmark> countQuery = queryFactory
